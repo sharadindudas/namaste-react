7c27055f8f48e1a46aae9865c557f0a51d7063f0
sharadindu das <sharadindudas774@gmail.com>
2025-01-27 16:25:14 +0530

    feat: Revised Chapter 03

---
+++ 03-Laying the Foundation/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element => JS Object => HTMLElement(render)
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React 🚀"
+);
+
+// JSX - HTML/XML like syntax
+const jsxHeading = <h1>Namaste React using JSX🚀</h1>;
+
+// JSX => React.createElement (Babel) => React Element => HTML Element (render)
+
+const Title = () => <h1>Namaste React using JSX</h1>;
+const HeadingComponent = () => (
+  <div className="container">
+    <Title />
+    <h2>Namaste React Functional Component</h2>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(<HeadingComponent />);
